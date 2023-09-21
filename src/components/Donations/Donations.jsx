12493f02dec835879f9b6f6f8303ce287aa2451d
carpicoder <webcarpi@gmail.com>
2023-09-21 08:14:19 +0200

    Logos de donaciones

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -5,6 +5,11 @@ import gsap from "gsap";
 import InnerSectionSubtitle from "../InnerSectionSubtitle";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useTranslation } from "react-i18next";
+import logoStripe from "../../assets/logo-stripe.png";
+import logoPaypal from "../../assets/logo-paypal.png";
+import logoCafecito from "../../assets/logo-cafecito.jpg";
+import logoMercadopago from "../../assets/logo-mercadopago.png";
+import logoPatreon from "../../assets/logo-patreon.png";
 
 
 const Donations = () => {
@@ -41,25 +46,45 @@ const Donations = () => {
           <article id="argentina" className="donation" ref={element => {donationRef.current[0] = element;}}>
             <h2 className="donation-title">{tt("donations.fromArgentina")}</h2>
 
-            <h3 className="donation-subtitle">MercadoPago <span>(ARS)</span></h3>
+            <h3 className="donation-subtitle">
+            <img src={logoCafecito} alt="Cafecito logo" />
+              Cafecito <span>(ARS)</span>
+              </h3>
+            <div className="buttons">
+              <a target="_blank" href="https://cafecito.app/carpicoder" className="button">{tt("donations.oneTime")}</a>
+            </div>
+
+            <h3 className="donation-subtitle">
+              <img src={logoMercadopago} alt="MercadoPago logo" />
+              MercadoPago <span>(ARS)</span>
+              </h3>
             <div className="buttons">
               <a target="_blank" href="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848a8a854e018a9546800b073f" className="button">{tt("donations.monthly")}</a>
               <a target="_blank" href="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848a6c207d018a708b1e20028b" className="button">{tt("donations.oneTime")}</a>
             </div>
 
-            <h3 className="donation-subtitle">PayPal <span>(USD)</span></h3>
+            <h3 className="donation-subtitle">
+              <img src={logoPaypal} alt="PayPal logo" />
+              PayPal <span>(USD)</span>
+              </h3>
             <p className="donation-text-small">{tt("donations.pesifies")}</p>
             <div className="buttons">
               <a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=BVJ6LD7YYYFY2" className="button">{tt("donations.monthlyOrOneTime")}</a>
             </div>
 
-            <h3 className="donation-subtitle">Stripe <span>(USD)</span></h3>
+            <h3 className="donation-subtitle">
+              <img src={logoStripe} alt="Stripe logo" />
+              Stripe <span>(USD)</span>
+              </h3>
             <p className="donation-text-small">{tt("donations.pesifies")}</p>
             <div className="buttons">
               <a target="_blank" href="https://donate.stripe.com/aEU5oifsc9oacRadQQ" className="button">{tt("donations.oneTime")}</a>
             </div>
 
-            <h3 className="donation-subtitle">Patreon <span>(USD)</span></h3>
+            <h3 className="donation-subtitle">
+              <img src={logoPatreon} alt="Patreon logo" />
+              Patreon <span>(USD)</span>
+              </h3>
             <p className="donation-text-small">{tt("donations.pesifies")}</p>
             <div className="buttons">
               <a target="_blank" href="https://patreon.com/carpicoder" className="button">{tt("donations.monthly")}</a>
@@ -69,19 +94,28 @@ const Donations = () => {
           <article id="resto-del-mundo" className="donation" ref={element => {donationRef.current[1] = element;}}>
             <h2 className="donation-title">{tt("donations.fromTheRest")}</h2>
 
-            <h3 className="donation-subtitle">PayPal</h3>
+            <h3 className="donation-subtitle">
+            <img src={logoPaypal} alt="PayPal logo" />
+              PayPal
+              </h3>
             <div className="buttons">
               <a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=BVJ6LD7YYYFY2" className="button">{tt("donations.monthlyOrOneTime")} (USD)</a>
               <a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=88DZ3PQXDCG38" className="button">{tt("donations.monthlyOrOneTime")} (EUR)</a>
             </div>
 
-            <h3 className="donation-subtitle">Stripe</h3>
+            <h3 className="donation-subtitle">
+              <img src={logoStripe} alt="Stripe logo" />
+              Stripe
+              </h3>
             <div className="buttons">
               <a target="_blank" href="https://donate.stripe.com/aEU5oifsc9oacRadQQ" className="button">{tt("donations.oneTime")} (USD)</a>
               <a target="_blank" href="https://donate.stripe.com/5kA2c6cg0gQC9EY289" className="button">{tt("donations.oneTime")} (EUR)</a>
             </div>
 
-            <h3 className="donation-subtitle">Patreon</h3>
+            <h3 className="donation-subtitle">
+              <img src={logoPatreon} alt="Patreon logo" />
+              Patreon
+              </h3>
             <div className="buttons">
               <a target="_blank" href="https://patreon.com/carpicoder" className="button">{tt("donations.monthly")} (USD)</a>
             </div>
--- src/components/Home/Hero/HeroStats.jsx
+++ src/components/Home/Hero/HeroStats.jsx
@@ -1,6 +1,7 @@
 import gsap from 'gsap';
-import React, { useLayoutEffect, useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next';
+import { youtubeKey, youtubeUser } from '../../../helpers/subs';
 
 const HeroStats = () => {
 
@@ -21,19 +22,53 @@ const HeroStats = () => {
     
     }, [i18n.language])
 
+    
+    const [subsData, setSubsData] = useState(null);
+    let SI_SYMBOL = ["", "K", "M", "G", "T", "P", "E"];
+    function abbreviateNumber(number){
+        var tier = Math.log10(Math.abs(number)) / 3 | 0;
+        if(tier == 0) return number;
+        var suffix = SI_SYMBOL[tier];
+        var scale = Math.pow(10, tier * 3);
+        var scaled = number / scale;
+        return scaled.toFixed(1) + suffix;
+    }
+    useEffect(() => {
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            if (data.items) {
+                setSubsData({
+                    subCount: abbreviateNumber(data.items[0].statistics.subscriberCount),
+                    videoCount: abbreviateNumber(data.items[0].statistics.videoCount),
+                    viewCount: abbreviateNumber(data.items[0].statistics.viewCount)
+                })
+            } else {
+                setSubsData({
+                    subCount: "+8K",
+                    videoCount: "+90",
+                    viewCount: "+350K"
+                })
+            }
+        })
+    }, [])
+
+
     return (
 
         <div className="hero-stats">
             <div className="hero-stat" ref={element => {statRef.current[0] = element;}}>
-                <p className="hero-stat-number">7.7K</p>
+                <p className="hero-stat-number">{subsData ? subsData.subCount : "8K"}</p>
                 <p className="hero-stat-text">{tt("home.stats.subscribers")}</p>
             </div>
             <div className="hero-stat" ref={element => {statRef.current[1] = element;}}>
-                <p className="hero-stat-number">90</p>
+                <p className="hero-stat-number">{subsData ? subsData.videoCount : "90"}</p>
                 <p className="hero-stat-text">{tt("home.stats.videos")}</p>
             </div>
             <div className="hero-stat" ref={element => {statRef.current[2] = element;}}>
-                <p className="hero-stat-number">357.7K</p>
+                <p className="hero-stat-number">{subsData ? subsData.viewCount : "350K"}</p>
                 <p className="hero-stat-text">{tt("home.stats.views")}</p>
             </div>
         </div>
